71e3df030e3208139c4b367046cea2b2ee5e1ce6
BlueprintAiConsulting <drewhufnagle@gmail.com>
2026-01-25 22:15:11 -0500

    Fix unterminated comment on line 60

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const App = () => (
               }
             />
             
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
